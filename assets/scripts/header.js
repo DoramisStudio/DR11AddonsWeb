5f8ac52454dea997100b805c08414fe5eb343925
DoramisStudio <mariarenny04@gmail.com>
2024-07-19 22:39:27 +0700

    Update

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,6 +1,10 @@
+const header_setting = {
+    current: ""
+};
+
 function checkHeaderScroll() {
     const hero_top = document.querySelector("div.hero");
-    
+
     if (window.scrollY > hero_top.clientHeight / 2) header.classList.add("filled");
     else header.classList.remove("filled");
 }
