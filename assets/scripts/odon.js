83af25e9b2ab03cbd039609085a3c9901932d904
DoramisStudio <mariarenny04@gmail.com>
2024-07-21 21:01:33 +0700

    Update

--- assets/scripts/odon.js
+++ assets/scripts/odon.js
@@ -18,22 +18,22 @@ function createIconElement(iconName) {
 function buildOdonList(json) {
 	odon_list.innerHTML = "";
 	json.forEach(meta => {
-		container = document.createElement("div");
+		const container = document.createElement("div");
 		container.classList = "list-item";
 
 		// Main left side (Thumbnail & Button actions)
-		lside = document.createElement("div");
+		const lside = document.createElement("div");
 		lside.classList = "lside";
 		// Thumbnail
-		thumb = document.createElement("img");
+		const thumb = document.createElement("img");
 		thumb.classList = "thumb";
 		thumb.src = meta.thumbnail != "" ? meta.thumbnail : nimg[Math.floor(Math.random() * nimg.length)];
 		lside.append(thumb);
 		// Button group
-		buttong = document.createElement("div");
+		const buttong = document.createElement("div");
 		buttong.classList = "button-group";
 		// Download Button
-		button_dl = document.createElement("button");
+		const button_dl = document.createElement("button");
 		button_dl.classList = "square aspect-free download-btn";
 		button_dl.title = "Download";
 		if (meta.url.download_url != "") {
@@ -47,7 +47,7 @@ function buildOdonList(json) {
 		button_dl.append(createIconElement("download"));
 		buttong.append(button_dl);
 		// Page Button
-		button_pg = document.createElement("button");
+		const button_pg = document.createElement("button");
 		button_pg.classList = "square aspect-free page-btn";
 		button_pg.title = "Open page";
 		if (meta.url.page_url != "") {
@@ -60,8 +60,46 @@ function buildOdonList(json) {
 		}
 		button_pg.append(createIconElement("open_in_new"));
 		buttong.append(button_pg);
+		// Append all
 		lside.append(buttong);
 		container.append(lside);
+
+		// Right side (Title, Status, etc)
+		const rdata = document.createElement("div");
+		rdata.classList = "data";
+		// Title
+		const title = document.createElement("h1");
+		title.classList = "title";
+		title.textContent = meta.title + " ";
+		// Status
+		const status = document.createElement("span");
+		status.classList = "status";
+		status.textContent = meta.status;
+		title.append(status);
+		rdata.append(title);
+		// Credits
+		const credits = document.createElement("div");
+		credits.classList = "credits";
+		meta.credits.forEach(cred => {
+			const sp = document.createElement("span");
+			sp.textContent = cred;
+			credits.append(sp);
+		});
+		rdata.append(credits);
+		// Description
+		const desc = document.createElement("p");
+		desc.classList = "desc";
+		desc.textContent = meta.description;
+		rdata.append(desc);
+		// Note
+		const note = document.createElement("ul");
+		note.classList = "note";
+		meta.note.forEach(nt => {
+			const li = document.createElement("li");
+			li.textContent = nt;
+			note.append(li);
+		});
+		rdata.append(note);
 
 		odon_list.append(container);
 	});
