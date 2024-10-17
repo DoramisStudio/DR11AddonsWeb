0fd6a1cc9f4b613fe831233dda37cd3511b07366
DoramisStudio <mariarenny04@gmail.com>
2024-10-17 11:20:24 +0700

    pages update + fix

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -18,12 +18,22 @@ function checkScroll() {
 	checkHeaderScroll();
 }
 
+function scrollToId(id) {
+	window.scrollTo(0, document.getElementById(id).getBoundingClientRect().y - header.getBoundingClientRect().height)
+}
+
 function Init() {
 	document.querySelector(".wrapper").style.overflow = "visible";
 	document.querySelector(".loadingscr").classList.add("hide");
 	hero_config.index = Math.floor(Math.random() * hero_imgs.length);
-	document.body.style = `--accent-color: ${hero_imgs[hero_config.index].accentColor}; --bg-grad-top: ${hero_imgs[hero_config.index].bg.top}; --bg-grad-bottom: ${hero_imgs[hero_config.index].bg.bottom};`;
+	document.body.style.setProperty("--accent-color", hero_imgs[hero_config.index].accentColor);
+	document.body.style.setProperty("--bg-grad-top", hero_imgs[hero_config.index].bg.top);
+	document.body.style.setProperty("--bg-grad-bottom", hero_imgs[hero_config.index].bg.bottom);
 	hero_top.children[0].src = hero_imgs[hero_config.index].src;
 	checkScroll();
 }
+
+window.onresize = (e) => {
+	document.body.style.setProperty("--zoom-level", window.devicePixelRatio);
+};
 // Desktop@RikoRiko
